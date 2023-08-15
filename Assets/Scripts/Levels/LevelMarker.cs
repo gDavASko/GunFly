cac8839ddd86e4f39e3bc6a8c5993c87890dd0b2
gDavASko <DavASko01@gmail.com>
2023-08-16 03:11:08 +0500

    - fix error

--- Assets/Scripts/Levels/LevelMarker.cs
+++ Assets/Scripts/Levels/LevelMarker.cs
@@ -7,9 +7,11 @@ public class LevelMarker : MonoBehaviour
     [SerializeField] private string _id = "marker";
     public string Id => _id;
 
+#if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         Handles.Label(transform.position + Vector3.up * 0.5f, name);
         Gizmos.DrawSphere(transform.position, 0.1f);
     }
+#endif
 }
